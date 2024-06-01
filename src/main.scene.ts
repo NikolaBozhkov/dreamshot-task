f6789af8db3df83e987bb968abe205c2a2b5b822
Nikola Bozhkov <nikola.bozhkov@payhawk.com>
2024-06-01 16:11:03 +0300

    feat: Add basic shine particles

--- src/main.scene.ts
+++ src/main.scene.ts
@@ -1,4 +1,4 @@
-import { Application, Container, FederatedPointerEvent, Sprite, Ticker } from 'pixi.js';
+import { Application, FederatedPointerEvent, LinearDodgeBlend, Sprite, Ticker } from 'pixi.js';
 
 import { VaultCombination, generateVaultCombination } from './vault-combination';
 import { textureMap } from './texture';
@@ -20,9 +20,12 @@ export class MainScene {
     private handle: Handle;
     private doorOpen: Sprite;
     private doorOpenShadow: Sprite;
+    private shineParticle: Sprite[];
 
     private prevDirection = 0;
     private didCheckLastCombination = true;
+
+    private sceneTime: number = 0;
 
     constructor(private readonly app: Application) {
         console.log(this.vaultCombination);
@@ -78,6 +81,31 @@ export class MainScene {
             x: this.app.renderer.width / 2,
             y: this.app.renderer.height / 2,
         }
+
+        this.shineParticle = [];
+        for (let i = 0; i < 3; i++) {
+            this.shineParticle.push(new Sprite({
+                texture: textureMap['blink'],
+                anchor: { x: 0.5, y: 0.5 },
+                scale: this.baseScale,
+                rotation: Math.random() * Math.PI * 2,
+            }));
+        }
+
+        this.shineParticle[0].position = {
+            x: app.renderer.width / 2 - 150,
+            y: app.renderer.height / 2,
+        };
+
+        this.shineParticle[1].position = {
+            x: app.renderer.width / 2 - 25,
+            y: app.renderer.height / 2 - 10,
+        };
+
+        this.shineParticle[2].position = {
+            x: app.renderer.width / 2 + 45,
+            y: app.renderer.height / 2 + 95,
+        };
     }
 
     init() {
@@ -87,6 +115,8 @@ export class MainScene {
         this.app.stage.addChild(this.background);
         this.app.stage.addChild(this.door);
         this.app.stage.addChild(this.handle);
+
+        this.app.ticker.add(this.updateSceneTime.bind(this));
 
         this.app.ticker.add(this.handle.updateRotation.bind(this.handle));
         this.app.ticker.add(this.checkCombinationMatch.bind(this));
@@ -138,11 +168,27 @@ export class MainScene {
 
         this.app.stage.removeChild(this.doorOpen);
         this.app.stage.removeChild(this.doorOpenShadow);
+        this.app.stage.removeChild(...this.shineParticle);
+
+        this.app.ticker.remove(this.updateShineParticles.bind(this));
 
         if (!this.handle.parent && !this.door.parent) {
             this.app.stage.addChild(this.door, this.handle);
         }
-    };
+    }
+
+    private openVault() {
+        this.app.stage.removeChild(this.door);
+        this.app.stage.removeChild(this.handle);
+
+        this.app.stage.addChild(this.doorOpen);
+        this.app.stage.addChild(this.doorOpenShadow);
+        this.app.stage.addChild(...this.shineParticle);
+
+        this.app.ticker.add(this.updateShineParticles.bind(this));
+
+        setTimeout(this.resetGame.bind(this), 5000);
+    }
 
     private checkCombinationMatch() {
         // Check only if the handle has 'clicked' in place
@@ -170,14 +216,23 @@ export class MainScene {
             const doesMatchLastPair = this.playerVaultCombination[this.playerVaultCombination.length - 1].count
                 == this.vaultCombination[this.vaultCombination.length - 1].count;
             if (this.playerVaultCombination.length == this.vaultCombination.length && doesMatchLastPair) {
-                this.app.stage.removeChild(this.door);
-                this.app.stage.removeChild(this.handle);
-                this.app.stage.addChild(this.doorOpen);
-                this.app.stage.addChild(this.doorOpenShadow);
-                setTimeout(this.resetGame.bind(this), 5000);
+                this.openVault();
             }
 
             this.didCheckLastCombination = true;
+        }
+    }
+
+    private updateSceneTime(ticker: Ticker) {
+        this.sceneTime += ticker.deltaTime;
+    }
+
+    private updateShineParticles(ticker: Ticker) {
+        for (let shineParticle of this.shineParticle) {
+            let deltaTimeSeconds = ticker.deltaMS * 0.001;
+            shineParticle.rotation += deltaTimeSeconds * Math.PI * 0.25;
+            let particleTimeOffset = shineParticle.position.x + shineParticle.position.y;
+            shineParticle.alpha = 0.6 + 0.4 * (0.5 + 0.5 * Math.sin(this.sceneTime * 0.001 * Math.PI * 13 + particleTimeOffset));
         }
     }
 }
